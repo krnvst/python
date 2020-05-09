20d8d787ce6f3a0917773a8d1c4d3b072e7e40d4
krnvsts <skorns20@gmail.com>
2020-05-09 23:30:19 +0300

    add basic file python

---
+++ basic/variable.py
@@ -0,0 +1,9 @@
+string_one = "Собака съела "
+string_two = "мою книгу!"
+string_three = string_one + string_two
+ 
+print(string_three) # Собака съела мою книгу!
+
+
+add='%d %s, %d %s' % (6, 'bananas', 10, 'lemons')
+print(add)
